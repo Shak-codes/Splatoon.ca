31a2fbb85eb2abde7d44bb7a14e6fe5b400ada25
Shakir <shakirrahman81@gmail.com>
2025-07-31 17:57:31 -0400

    Adds other pages

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -10,11 +9,13 @@ const navLinks = [
   { title: "FAQ", href: "/faq" },
 ];
 
-export default function Home() {
+const Events = () => {
   return (
     <div className="font-sans grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen pb-20">
       <Navbar config={navLinks} />
       <Footer />
     </div>
   );
-}
+};
+
+export default Events;
