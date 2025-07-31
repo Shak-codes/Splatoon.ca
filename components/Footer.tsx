f200b7c7a7f89a56ae3788253f6a67c126d5cc2b
Shakir <shakirrahman81@gmail.com>
2025-07-31 17:49:45 -0400

    Adds footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = ({}) => {
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap text-center px-6">
+      Splatoon.ca is an independent, fan-run website dedicated to organizing
+      Splatoon events and connecting players in the Greater Toronto Area. This
+      site is not affiliated with, sponsored by, or endorsed by Nintendo Co.,
+      Ltd. or any of its subsidiaries. All Splatoon™ and Nintendo® trademarks
+      and images are the property of Nintendo.
+    </footer>
+  );
+};
+
+export default Footer;
