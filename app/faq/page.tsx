064f25c3dca97876342c8719809a18b52b3efb4f
Shakir <shakirrahman81@gmail.com>
2025-08-05 07:38:05 -0400

    Updates FAQ questions

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -16,14 +16,32 @@ const testFAQ = [
     answer:
       "There are absolutely no skill level requirements to participate in our events. We run both competitive and casual matches during our events so all skill levels will be able to enjoy!",
   },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
-  { question: "What should I bring?", answer: "Bring XYZ!" },
+  {
+    question: "Is food allowed in the venue?",
+    answer:
+      "Invictus doesn't allow any outside food or drinks inside the venue aside from a water bottle. That being said, Invictus does sell their own food and drinks within the venue.",
+  },
+  {
+    question:
+      "Can my parents attend? If so, do they have to pay the entry fee?",
+    answer:
+      "Parents are free to attend and they do not have to pay the entry fee as long as they're not playing.",
+  },
+  {
+    question: "Does Invictus have on-site parking?",
+    answer:
+      "Invictus does not have on-site parking, however, there are many parking garages around the area.",
+  },
+  {
+    question: "Should I bring my Switch 1 or Switch 2?",
+    answer:
+      "Please bring one of your Switch 1 or Switch 2. It doesn't matter which console you bring, just that you bring one with a copy of Splatoon 3.",
+  },
+  {
+    question: "Do I have to show up at noon?",
+    answer:
+      "Feel free to join us at any time. We usually stop pllaying matches around 5 to sign posters and take pictures though so don't be too late!",
+  },
 ];
 
 const FAQPage = () => {
