f63694bc50f31a58377fb44e2ea63c08c81b1720
Shakir <shakirrahman81@gmail.com>
2025-08-10 19:02:09 -0400

    Adds nav constants file

---
+++ public/constants/nav.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { title: "HOME", href: "/" },
+  { title: "ABOUT", href: "/about" },
+  { title: "EVENTS", href: "/events" },
+  { title: "CONNECT", href: "/connect" },
+  { title: "FAQ", href: "/faq" },
+];
