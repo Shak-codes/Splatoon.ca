4f34d13790ff4176eac65c462d2711c1cc60ddbf
Shakir <shakirrahman81@gmail.com>
2025-08-16 14:52:36 -0400

    Removes connect page

--- public/constants/nav.ts
+++ public/constants/nav.ts
@@ -6,7 +6,6 @@ export const navLinks = [
   { title: "HOME", href: "/" },
   { title: "ABOUT", href: "/about" },
   { title: "EVENTS", href: "/events" },
-  { title: "CONNECT", href: "/connect" },
   { title: "FAQ", href: "/faq" },
 ];
 
