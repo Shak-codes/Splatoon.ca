a88381c47843fbbc20952a8cd81c9e3bc6993ea8
Shakir <shakirrahman81@gmail.com>
2025-10-01 15:26:55 -0400

    Adds Typography component

---
+++ components/Typography/Typography.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.css";
+
+type TypographyProps = {
+  variant:
+    | "title"
+    | "subtitle"
+    | "sectionTitle"
+    | "sectionSubtitle"
+    | "subsectionTitle"
+    | "subsectionSubtitle"
+    | "paragraph";
+  children: ReactNode;
+  className?: string;
+};
+
+const Typography = ({ variant, children, className = "" }: TypographyProps) => {
+  const Tag =
+    variant === "title"
+      ? "h1"
+      : variant === "subtitle"
+      ? "h2"
+      : variant === "sectionTitle"
+      ? "h3"
+      : variant === "sectionSubtitle"
+      ? "h4"
+      : variant === "subsectionTitle"
+      ? "h5"
+      : variant === "subsectionSubtitle"
+      ? "h6"
+      : "p";
+
+  return (
+    <Tag className={`${styles.typography} ${styles[variant]} ${className}`}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Typography;
