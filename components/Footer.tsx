b1c7cd50aa5971d155156b103447c9533b8b0e17
Shakir <shakirrahman81@gmail.com>
2025-10-01 20:07:02 -0400

    Updates Footer to use new Typography

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,15 @@
+import Typography from "./Typography/Typography";
+
 const Footer = ({}) => {
   return (
-    <footer className="bottom-5 shrink-0 text-center px-5">
-      <p className="text-white">
+    <footer className="bottom-5 text-center p-5">
+      <Typography variant="paragraph">
         Splatoon.ca is an independent, fan-run website dedicated to organizing
         Splatoon events and connecting players in the Greater Toronto Area. This
         site is not affiliated with, sponsored by, or endorsed by Nintendo Co.,
         Ltd. or any of its subsidiaries. All Splatoon™ and Nintendo® trademarks
         and images are the property of Nintendo.
-      </p>
+      </Typography>
     </footer>
   );
 };
