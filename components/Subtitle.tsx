0005ff4d0cdb59bf75e7e48955db0adcfa55505f
Shakir <shakirrahman81@gmail.com>
2025-10-06 22:46:36 -0400

    Updates Subtitle to use Typography

---
+++ components/Subtitle.tsx
@@ -0,0 +1,33 @@
+import Typography from "./Typography/Typography";
+
+type SubtitleProps = {
+  subtitle: string;
+  delay?: number;
+  className?: string;
+};
+
+const Subtitle = ({ subtitle, delay = 0 }: SubtitleProps) => {
+  return (
+    <Typography
+      variant="subtitle"
+      size="text-lg"
+      className="rounded-lg bg-white p-2 !text-[var(--secondary-background)] text-center"
+    >
+      <span key={subtitle}>
+        {subtitle.split("").map((c, i) => (
+          <span
+            key={i}
+            className="animate-subtitle"
+            style={{
+              animationDelay: `${delay + i * 0.08}s`,
+            }}
+          >
+            {c === " " ? "\u00A0" : c}
+          </span>
+        ))}
+      </span>
+    </Typography>
+  );
+};
+
+export default Subtitle;
