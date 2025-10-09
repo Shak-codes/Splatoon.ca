15d07764f28c647349ffd7908cffba2e1ef71335
Shakir <shakirrahman81@gmail.com>
2025-10-09 16:05:52 -0400

    Button styling updates

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,36 +1,41 @@
 import React from "react";
+import Link from "next/link";
 
 type ButtonProps = {
   text: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled: boolean;
+  href?: string;
+  bghover?: "bg" | "accent";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = ({ text, onClick, disabled }: ButtonProps) => {
+const Button = ({
+  text,
+  disabled,
+  href,
+  bghover = "bg",
+  onClick,
+}: ButtonProps) => {
+  const className = `
+    flex justify-center items-center
+    bg-transparent border border-white text-white font-bold
+    h-12 w-full rounded cursor-pointer
+    transition-colors duration-300
+    ${bghover === "accent" && "hover:bg-[var(--accent)]"}
+    ${bghover === "bg" && "hover:bg-[var(--primary)]"} 
+    hover:border-transparent 
+  `;
+  if (href) {
+    return (
+      <Link href={href} className={className}>
+        {text}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      className="
-        flex
-        justify-center
-        items-center
-        bg-transparent
-        border
-        border-white
-        text-white
-        font-bold
-        h-12
-        w-full
-        rounded
-        m-0
-        ease-in-out
-        cursor-pointer
-        transition-colors duration-300 ease-in-out
-        hover:bg-white
-        hover:text-[var(--secondary-background)]
-    "
-      disabled={disabled}
-    >
-      <div className="">{text}</div>
+    <button className={className} disabled={disabled} onClick={onClick}>
+      {text}
     </button>
   );
 };
