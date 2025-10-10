215b78feea30bfeda93ec5f8a714c8a9307d384f
Shakir <shakirrahman81@gmail.com>
2025-10-10 13:09:26 -0400

    Removes ImageCarousel styling file

--- components/ImageCarousel/ImageCarousel.tsx
+++ components/ImageCarousel/ImageCarousel.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import styles from "./styles.module.css";
 
 interface ImageCarouselProps {
   images: { src: string; width: number; height: number }[];
