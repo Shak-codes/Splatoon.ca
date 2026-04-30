eb9ca38e5f48628b598b9bb1a68bf05505f6f118
IceFlinger <ice@loud.house>
2026-04-30 19:36:19 -0400

    Update EventHeader.tsx

--- components/EventHeader/EventHeader.tsx
+++ components/EventHeader/EventHeader.tsx
@@ -92,7 +92,7 @@ const EventHeader = ({ date, location, href, className }: EventHeaderProps) => {
           heart of the Toronto. Whether you&apos;re a long time player or new to
           the community, these events are the perfect way to connect with fellow
           fans from around the GTA. Mark your calendars, the next Squid Social
-          is officially locked in for Sunday April 19th! Join us for an
+          is officially locked in for Sunday May 31st! Join us for an
           afternoon of fun, laughter and great vibes at Squid Social{" "}
           {eventNumber}!
         </Typography>
